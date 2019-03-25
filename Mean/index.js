0cd5d6ac0df9fd6d56ad1c178a22d87cb8382b07
PeeJoy Gomba <peejoy4@gmail.com>
2019-03-25 11:56:15 +0200

    My files

---
+++ Mean/index.js
@@ -0,0 +1,9 @@
+var one = 1
+var two = 2
+var three = one * two
+
+var four = [1,2,3,4,5,6,7,8,9,10]
+for (var i = 0; i < four.length; i++) {
+	console.log("the index is node " + i + " the value is " + four[i]);
+}
+
